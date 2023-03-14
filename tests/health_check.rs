d598922382f7a05fa828d07b6b7ea0e0b547cdbd
Bruno Paulino <bruno.paulino@n26.com>
2023-03-14 20:32:24 +0100

    Integration tests for health check endpoint

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,6 +1,22 @@
-use mailbolt::main;
+#[tokio::test]
+async fn health_check() {
+    spawn_app();
 
-#[test]
-fn dummy_test() {
-    main()
+    let client = reqwest::Client::new();
+
+    let response = client
+        .get("http://127.0.0.1:8000/health_check")
+        .send()
+        .await
+        .expect("Failed to get /health_check endpoint");
+
+    assert!(response.status().is_success());
+    assert_eq!(Some(0), response.content_length());
+}
+
+// Spawn our web server in the background so we can execute
+// the web server and our tests concurrently.
+fn spawn_app() {
+    let server = mailbolt::run().expect("failed to bind address");
+    let _ = tokio::spawn(server);
 }
