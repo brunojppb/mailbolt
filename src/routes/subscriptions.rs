7c5f82d86cf4b7de74eb62ec99dbf139025f88b8
Bruno Paulino <bruno@bpaulino.com>
2023-05-06 11:21:37 +0200

    Use strong types to only have valid subscriber names

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -3,6 +3,8 @@ use chrono::Utc;
 use sqlx::PgPool;
 use uuid::Uuid;
 
+use crate::domain::{NewSubscriber, SubscriberName};
+
 #[derive(serde::Deserialize)]
 pub struct FormData {
     pub name: String,
@@ -20,7 +22,12 @@ pub struct FormData {
     )
 ]
 pub async fn subscribe(form: web::Form<FormData>, conn_pool: web::Data<PgPool>) -> HttpResponse {
-    match insert_subscriber(&conn_pool, &form).await {
+    let new_subscriber = NewSubscriber {
+        email: form.0.email,
+        name: SubscriberName::parse(form.0.name),
+    };
+
+    match insert_subscriber(&conn_pool, &new_subscriber).await {
         Ok(_) => {
             tracing::info!("New subscriber has been saved");
             HttpResponse::Ok().finish()
@@ -34,16 +41,19 @@ pub async fn subscribe(form: web::Form<FormData>, conn_pool: web::Data<PgPool>) 
     }
 }
 
-#[tracing::instrument(name = "Inserting new sub details to DB", skip(pool, form))]
-pub async fn insert_subscriber(pool: &PgPool, form: &FormData) -> Result<(), sqlx::Error> {
+#[tracing::instrument(name = "Inserting new sub details to DB", skip(pool, subscriber))]
+pub async fn insert_subscriber(
+    pool: &PgPool,
+    subscriber: &NewSubscriber,
+) -> Result<(), sqlx::Error> {
     sqlx::query!(
         r#"
     INSERT INTO subscriptions (id, email, name, subscribed_at)
     VALUES ($1, $2, $3, $4)
     "#,
         Uuid::new_v4(),
-        form.email,
-        form.name,
+        subscriber.email,
+        subscriber.name.as_ref(),
         Utc::now()
     )
     .execute(pool)
