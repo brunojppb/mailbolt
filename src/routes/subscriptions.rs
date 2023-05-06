66cabbdd66a9459cd61e8e1698f23ff9f19b0e08
Bruno Paulino <bruno@bpaulino.com>
2023-05-06 12:32:50 +0200

    Use TryFrom to simplify request form data parsing

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -5,12 +5,6 @@ use uuid::Uuid;
 
 use crate::domain::{NewSubscriber, SubscriberEmail, SubscriberName};
 
-#[derive(serde::Deserialize)]
-pub struct FormData {
-    pub name: String,
-    pub email: String,
-}
-
 #[
     tracing::instrument(
         name = "Adding a new subscriber",
@@ -22,17 +16,10 @@ pub struct FormData {
     )
 ]
 pub async fn subscribe(form: web::Form<FormData>, conn_pool: web::Data<PgPool>) -> HttpResponse {
-    let name = match SubscriberName::parse(form.0.name) {
-        Ok(name) => name,
+    let new_subscriber = match form.0.try_into() {
+        Ok(subscriber) => subscriber,
         Err(_) => return HttpResponse::BadRequest().finish(),
     };
-
-    let email = match SubscriberEmail::parse(form.0.email) {
-        Ok(email) => email,
-        Err(_) => return HttpResponse::BadRequest().finish(),
-    };
-
-    let new_subscriber = NewSubscriber { email, name };
 
     match insert_subscriber(&conn_pool, &new_subscriber).await {
         Ok(_) => {
@@ -71,4 +58,20 @@ pub async fn insert_subscriber(
     })?;
 
     Ok(())
+}
+
+#[derive(serde::Deserialize)]
+pub struct FormData {
+    pub name: String,
+    pub email: String,
+}
+
+impl TryFrom<FormData> for NewSubscriber {
+    type Error = String;
+
+    fn try_from(value: FormData) -> Result<Self, Self::Error> {
+        let name = SubscriberName::parse(value.name)?;
+        let email = SubscriberEmail::parse(value.email)?;
+        Ok(Self { email, name })
+    }
 }
