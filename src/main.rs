6b221eaa7c7ccaee8ea471f74f663c2c03dbb60d
Bruno Paulino <bruno@bpaulino.com>
2023-05-07 21:54:27 +0200

    Extract server build and startup logic

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,8 @@
 use mailbolt::{
     configuration::get_configuration,
-    email_client::EmailClient,
-    startup::run,
+    startup::build,
     telemetry::{get_subscriber, init_subscriber},
 };
-
-use sqlx::postgres::PgPoolOptions;
-use std::{net::TcpListener, time::Duration};
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
@@ -14,31 +10,8 @@ async fn main() -> Result<(), std::io::Error> {
     let subscriber = get_subscriber("mailbolt".into(), "info".into(), std::io::stdout);
     init_subscriber(subscriber);
 
-    let config = get_configuration().expect("Could not read configuration file");
-
-    let conn_pool = PgPoolOptions::new()
-        .acquire_timeout(Duration::from_secs(2))
-        .connect_lazy_with(config.database.with_db());
-
-    let sender_email = config.email_client.sender().expect("Invalid sender email");
-    let email_client = EmailClient::new(
-        config.email_client.base_url,
-        sender_email,
-        config.email_client.auth_token,
-    );
-
-    let listener = TcpListener::bind(format!(
-        "{}:{}",
-        config.application.host, config.application.port
-    ))
-    .unwrap_or_else(|_| {
-        panic!(
-            "Could not bind server to port '{}'",
-            config.application.port
-        )
-    });
-
-    tracing::info!("Server started on port {}", config.application.port);
-
-    run(listener, conn_pool, email_client)?.await
+    let config = get_configuration().expect("Could not read configuration YML files");
+    let server = build(config).await?;
+    server.await?;
+    Ok(())
 }
--- src/startup.rs
+++ src/startup.rs
@@ -1,9 +1,12 @@
 use actix_web::dev::Server;
 use actix_web::{web, App, HttpServer};
+use sqlx::postgres::PgPoolOptions;
 use sqlx::PgPool;
 use std::net::TcpListener;
+use std::time::Duration;
 use tracing_actix_web::TracingLogger;
 
+use crate::configuration::Settings;
 use crate::email_client::EmailClient;
 use crate::routes::{health_check, subscribe};
 
@@ -31,4 +34,32 @@ pub fn run(
     .run();
 
     Ok(server)
+}
+
+pub async fn build(config: Settings) -> Result<Server, std::io::Error> {
+    let conn_pool = PgPoolOptions::new()
+        .acquire_timeout(Duration::from_secs(2))
+        .connect_lazy_with(config.database.with_db());
+
+    let sender_email = config.email_client.sender().expect("Invalid sender email");
+    let email_client = EmailClient::new(
+        config.email_client.base_url,
+        sender_email,
+        config.email_client.auth_token,
+    );
+
+    let listener = TcpListener::bind(format!(
+        "{}:{}",
+        config.application.host, config.application.port
+    ))
+    .unwrap_or_else(|_| {
+        panic!(
+            "Could not bind server to port '{}'",
+            config.application.port
+        )
+    });
+
+    tracing::info!("Server started on port {}", config.application.port);
+
+    run(listener, conn_pool, email_client)
 }
